07b71342c46d10c0605b77048bc1d41d44e1c628
RomuloBenica <romulobenica@bol.com.br>
2020-06-04 08:03:54 -0300

    Inclusao de checkbox habilitar email

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const electron = require('electron')
+const electron = require('electron');
+const fs = require('fs');
 
 const { app, BrowserWindow } = require('electron')
 
@@ -23,13 +24,35 @@ function configurar(){
   var segundoEmail = document.getElementById("email_2").value;
   let status = document.getElementById("status");
 
-  if(getway != "" || primeiroEmail != "" || segundoEmail != "" ){
+  fs.writeFile('./getway.txt', ''+getway,{enconding:'utf-8',flag: 'a'}, function (err) {
+    if (err) throw err;
+    status.innerHTML = "Configuração enviada com sucesso!!";
+  });
+  fs.writeFile('./primeiroemail.txt', ''+primeiroEmail,{enconding:'utf-8',flag: 'a'}, function (err) {
+    if (err) throw err;
+    status.innerHTML = "Configuração enviada com sucesso!!";
+  });
+  fs.writeFile('./segundoemail.txt', ''+segundoEmail,{enconding:'utf-8',flag: 'a'}, function (err) {
+    if (err) throw err;
+    status.innerHTML = "Configuração enviada com sucesso!!";
+  });
+  
+  if(getway != "" && primeiroEmail != "" && segundoEmail != "" ){
     status.innerHTML = "Configuração enviada:";
   }else {
     status.innerHTML = "Erro nas informacoes Digitadas";
-  }
+  };
 
 } 
+
+function haverificaChecboxb(){
+	if(document.getElementById('chip').checked){
+    document.getElementById('getway').disabled = true;
+    
+	} else{
+		document.getElementById('getway').disabled = false;
+	};
+}
 
 
 app.whenReady().then(createWindow)
