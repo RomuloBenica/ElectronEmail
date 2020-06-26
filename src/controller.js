f0ee3c2d61f23077cebc6bec1724b17d4fbfdb94
RomuloBenica <romulobenica@hotmail.com>
2020-06-26 08:26:31 -0300

    add controler

--- src/controller.js
+++ src/controller.js
@@ -1,37 +1,48 @@
-var gateway = document.getElementById('gateway');
-var primeiroEmail = document.getElementById("email_1");
-var segundoEmail = document.getElementById("email_2");
-
 
 function verificaChecboxbGateway(){
-	if(document.getElementById('chip').checked){
-          document.getElementById('gateway').disabled = true;
-          document.getElementById('gateway').value = "";
-	} else{
-	  document.getElementById('gateway').disabled = false;
-  };
-  if(document.getElementById('fibra').checked){
-          document.getElementById('gateway').disabled = false;
-	} else{
-          document.getElementById('gateway').disabled = true;
+  if(document.getElementById('habilitarRede').checked){
+        document.getElementById('gateway').disabled = false;
+        setButton()
+  } else{
+        document.getElementById('gateway').disabled = true;
+        document.getElementById('gateway').value = "";
+        setButton()
   };
 }
 
 function verificaChecboxbEmail(){
-	if(document.getElementById('emailNao').checked){
-          document.getElementById('email_1').disabled = true;
-          document.getElementById('email_2').disabled = true;
-          document.getElementById('email_1').value = "";
-          document.getElementById('email_2').value = "";
-	} else{
-          document.getElementById('email_1').disabled = false;
-          document.getElementById('email_2').disabled = false;
-  };
-  if(document.getElementById('emailSim').checked){
-          document.getElementById('email_1').disabled = false;
-          document.getElementById('email_2').disabled = false;
-	} else{
-          document.getElementById('email_1').disabled = true;
-          document.getElementById('email_2').disabled = true;
-  };
+        
+        if(document.getElementById('habilitarEmail').checked){
+                document.getElementById('email_1').disabled = false;
+                document.getElementById('email_2').disabled = false;
+                setButton()
+        }else {
+                document.getElementById('email_1').disabled = true;
+                document.getElementById('email_2').disabled = true;
+                document.getElementById('email_1').value = "";
+                document.getElementById('email_2').value = "";
+                setButton()
+        }
 }
+
+function verificaCheckboxID(){
+        if(document.getElementById('habilitarID').checked){
+          document.getElementById('idCentral').disabled = false;
+          setButton()
+        }else {
+          document.getElementById('idCentral').disabled = true;
+          document.getElementById('idCentral').value = "";
+          setButton()
+        }
+}
+
+function setButton (){
+        let status = document.getElementById("status");
+        if(status != ""){
+          status.innerHTML = "";
+          document.getElementById('enviar').value = "Configurar";
+        }
+}
+
+    
+  
